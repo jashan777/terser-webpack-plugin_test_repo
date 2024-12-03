ad75648bf3c363263f81b6fa3cf2070acb6fb41c
Jashandeep Singh <49744054+jashan777@users.noreply.github.com>
2024-12-03 09:49:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Adjust the path as necessary
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // Ensure the entry point is correct
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Add support for both .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"], // Allow importing without specifying file extensions
+  },
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"), // Adjust the path if necessary
+    }),
+    new webpack.ProgressPlugin(),
+  ],
+};
