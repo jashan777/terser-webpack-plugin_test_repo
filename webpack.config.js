7d8a4363c813eab528f2745c0e99e72f0b7632bc
Jashandeep Singh <49744054+jashan777@users.noreply.github.com>
2024-12-03 10:32:53 +0000

    feat: update webpack config to handle terser minification and chunking

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js", // Ensure the entry point is correct
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
+    chunkFilename: "[name].[contenthash].js",
+    assetModuleFilename: "[name].[contenthash][ext]",
   },
   module: {
     rules: [
@@ -31,6 +34,59 @@ module.exports = {
   devServer: {
     compress: true,
     allowedHosts: "all",
+  },
+  devtool: "source-map",
+  optimization: {
+    runtimeChunk: {
+      name: (entrypoint) => `runtime-${entrypoint.name}`,
+    },
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    usedExports: true,
+    sideEffects: true,
+    flagIncludedChunks: true,
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        default: false,
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+          priority: 5,
+          enforce: true,
+          filename: "vendor.[contenthash].js",
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: false,
+        terserOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+            drop_console: true,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+          sourceMap: true,
+        },
+      }),
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
